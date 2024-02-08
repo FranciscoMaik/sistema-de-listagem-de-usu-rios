fc4d2206175b553a1b8b0f69cf6656962c84583e
franciscomaik <chiconunes3081@gmail.com>
2024-02-08 01:47:37 -0300

    feat(server): adding cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,7 +1,13 @@
 import fastify from "fastify";
 import { usersRoutes } from "./routes/users";
+import cors from '@fastify/cors'
 
 const app = fastify()
+
+app.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+})
 
 app.register(usersRoutes, {
     prefix: "users"
