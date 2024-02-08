49e3c5de9859c93cbea3c56a43398844542cedae
franciscomaik <chiconunes3081@gmail.com>
2024-02-07 22:51:43 -0300

    feat(database): create table users

--- Backend/src/database.ts
+++ Backend/src/database.ts
@@ -1,12 +1,18 @@
-import { knex as settingKnex} from "knex"
+import { knex as settingKnex, Knex} from "knex"
 
-export const knex = settingKnex({
+export const config: Knex.Config = {
     client: "mysql",
     connection: {
         host: "127.0.0.1",
         port: 3306,
         user: "root",
         password: "testmanole",
-        database: "manole"
+        database: "manoletest"
+    },
+    migrations: {
+        extension: "ts",
+        directory: "./db/migrations"
     }
-})
+}
+
+export const knex = settingKnex(config)
